420f0990d5691e464f5d30af138f80f1357aa5bd
Josie Proto <josieproto0@gmail.com>
2025-10-15 23:15:47 +0200

    Adding supabase script and imports to files and updating supabase key

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
-<script src="https://skixfenlotxgocdoqxrx.supabase.co"></script>
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.5/dist/umd/supabase.min.js"></script>
 
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://skixfenlotxgocdoqxrx.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNraXhmZW5sb3R4Z29jZG9xeHJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1NDE2ODUsImV4cCI6MjA3NjExNzY4NX0.6c4gIUPcxdCDdRmXDTGRXpGngkLG7Uuak7-WMDRcAv0
 const supabase = createClient(supabaseUrl, supabaseKey)
