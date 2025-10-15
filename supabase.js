ec297860bc4810989cff391faa8ebfcac4dfbfce
Josie Proto <josieproto0@gmail.com>
2025-10-15 23:36:47 +0200

    Updated registration and login to flow with supabase

--- supabase.js
+++ supabase.js
@@ -3,5 +3,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://skixfenlotxgocdoqxrx.supabase.co'
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNraXhmZW5sb3R4Z29jZG9xeHJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1NDE2ODUsImV4cCI6MjA3NjExNzY4NX0.6c4gIUPcxdCDdRmXDTGRXpGngkLG7Uuak7-WMDRcAv0
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNraXhmZW5sb3R4Z29jZG9xeHJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1NDE2ODUsImV4cCI6MjA3NjExNzY4NX0.6c4gIUPcxdCDdRmXDTGRXpGngkLG7Uuak7-WMDRcAv0';
 const supabase = createClient(supabaseUrl, supabaseKey)
